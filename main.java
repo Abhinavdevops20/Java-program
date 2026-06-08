73f18463a91f77d7ced9ff11ced5f62ec1e234fb
abhinavdevops20 <rawalabhinav20@gmail.com>
2026-06-09 05:12:42 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("This is a java program!");
+        System.out.println("Output for java program!");
+    }
+}
