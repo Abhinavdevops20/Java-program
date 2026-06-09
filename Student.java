1735f0cc55847644c3863cfcc0cda196a01df9c1
abhinavdevops20 <rawalabhinav20@gmail.com>
2026-06-10 04:14:25 +0530

    added Student class

---
+++ Student.java
@@ -0,0 +1,21 @@
+class Student {
+    String name;
+    int age;
+    int rollNo;
+
+    void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Roll Number: " + rollNo);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.name = "Abhinav";
+        s1.age = 25;
+        s1.rollNo = 101;
+
+        s1.displayDetails();
+    }
+}
